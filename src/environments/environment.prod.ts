e4605b890092bc6793d6c4f0d7647a0394496822
Deepak-Nexa <deepak.k@officeinfosystems.com>
2026-02-20 10:20:39 +0530

    Exe Build Error Fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   secretkey:'ce6834aa-049f-46a8-806c-fb36cdf22d84',
 
   // Dev environment
-  // apiBaseUrl: 'https://www.nexaois.com/OIS_Vault_API_Dev/api',
-  // ssoApiBaseUrl: 'https://www.nexaois.com/OIS_SSO_API_Dev/api'
+  apiBaseUrl: 'https://www.nexaois.com/OIS_Vault_API_Dev/api',
+  ssoApiBaseUrl: 'https://www.nexaois.com/OIS_SSO_API_Dev/api'
 
   // QA environment
   // apiBaseUrl: 'https://www.nexaois.com/OIS_Vault_API_QA/api',
